200360a4a15f76993d5763ec21156890c056377a
Vue-React <105972856+Vue-React@users.noreply.github.com>
2022-06-30 18:01:44 +0300

    Add files via upload

---
+++ src/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: {
+      // // для работы autoprefixer нужно добавлять настройки в webpack.config.js
+      // autoprefixer: {},
+      // оптимизация (минификация) css:
+      cssnano: {},
+    }
+  };
