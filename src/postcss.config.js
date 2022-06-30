dc3bb612c9176ed7f196588b4d484811b84eca63
Vue-React <105972856+Vue-React@users.noreply.github.com>
2022-06-30 18:09:52 +0300

    Delete src directory

--- src/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: {
-      // // для работы autoprefixer нужно добавлять настройки в webpack.config.js
-      // autoprefixer: {},
-      // оптимизация (минификация) css:
-      cssnano: {},
-    }
-  };
